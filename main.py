099b5cf5c39b8c68b8586626320e6840649eb293
adinoself <94802252+adinoself@users.noreply.github.com>
2023-02-21 09:54:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "transcription", "abcdef")
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
 
 name, authentication_status, username = authenticator.login("Login", "main")
 
